386945106c2f627aeb483818db18af00bb272024
mahimaquadras <145350652+mahimaquadras@users.noreply.github.com>
2024-11-10 10:03:11 +0530

    Update index.jsx

--- client/src/components/ChefSidebar/index.jsx
+++ client/src/components/ChefSidebar/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './ChefSidebar.css'; // Include CSS for chef sidebar styling
 
 const ChefSidebar = () => {
     return (
@@ -11,8 +10,7 @@ const ChefSidebar = () => {
                 <li><Link to="/chef-dashboard/recipes">Recipes</Link></li>
                 <li><Link to="/orders">Orders</Link></li>
                 <li><Link to="/inventory">Inventory</Link></li>
-                {/* Add other Chef-specific links here */}
-            </ul>
+               
         </div>
     );
 };
