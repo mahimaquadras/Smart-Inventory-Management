d3c6de4d08bac339e0f5f6b3f453927d5318fb92
mahimaquadras <145350652+mahimaquadras@users.noreply.github.com>
2024-11-10 10:05:18 +0530

    Update index.jsx

--- client/src/components/Main/index.jsx
+++ client/src/components/Main/index.jsx
@@ -1,28 +1,4 @@
-// import styles from './styles.module.css';
-// import SalesGraph from '../salesGraph';
 
-// const Main = () => {
-
-//     // const handleLogout = () => {
-//     //     localStorage.removeItem("token");
-//     //     window.location.reload();
-//     // };
-//     return(
-//         <div className={styles.main_container}>
-//             {/* <nav className={styles.navbar}>
-//                 <h1>PQ Inventory</h1>, */}
-//                 <SalesGraph />
-//                 {/* <button className={styles.white_btn} onClick={handleLogout}>
-//                     Logout
-//                 </button> */}
-//             {/* </nav> */}
-//         </div>
-//     )
-// };
-
-// export default Main;
-
-// src/components/Main.js
 import React from 'react';
 import Sidebar from "../Sidebar/sidebar";
 import SalesGraph from '../salesGraph';
